b5524219926dab1a6d9185d32f9f64c703b58c5d
Sarjith <rmsarju@gmail.com>
2021-01-18 01:57:50 +0530

    PART-C Development 1. Edit Posts 2. Like Post 3. Comment Post

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -12,3 +12,12 @@ function deletePost(){
     post.remove();
     closeModal('deleteModal');
 }
+
+function postDetails(id){
+    post = document.getElementById(id);
+    sessionStorage.clear();
+    sessionStorage.setItem("user", post.getElementsByClassName("postUser")[0].getElementsByTagName("p")[0].textContent);
+    sessionStorage.setItem("title", post.getElementsByClassName("postTitle")[0].getElementsByTagName("p")[0].textContent);
+    sessionStorage.setItem("description", post.getElementsByClassName("postDescription")[0].textContent);
+    window.location.href = "../html/post.html";
+}
